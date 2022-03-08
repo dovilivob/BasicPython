c2f6d4202ddce11a2a1b1e179681a03bfb4160ce
PC <david.iii.chiu@gmail.com>
2022-03-08 22:17:39 +0800

    first commit

---
+++ .py
@@ -0,0 +1,17 @@
+
+
+int = 6
+string = "hello"
+double = 3.9
+boolean = True
+list = ["hello", "you", "fucking", "cunt", "!!"]
+tuple = (3, 3, 2)
+
+# for n in list:
+#     print(n)
+
+# for i in range(len(list)):
+#     print(list[i])
+
+# for index, element in enumerate(list):
+#     print(index, element)
